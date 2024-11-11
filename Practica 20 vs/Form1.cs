d7495c3aaa269b7e922fe2b3f7a63e4b09616f70
Alumnos <Alumnos@DESKTOP-OHBOQ9P>
2024-11-11 12:38:47 -0600

    Agregar archivos de proyecto.

---
+++ Practica 20 vs/Form1.cs
@@ -0,0 +1,25 @@
+namespace Practica_20_vs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double cal1, cal2, cal3, cal4, prom;
+            cal1 = double.Parse(textBox1.Text);
+            cal2 = double.Parse(textBox2.Text);
+            cal3 = double.Parse(textBox3.Text);
+            cal4 = double.Parse(textBox4.Text);
+
+            prom = (cal1 + cal2 + cal3 + cal4) / 4;
+
+            if (prom > 6) MessageBox.Show(" APROBADO ");
+            else
+                if (prom < 6) MessageBox.Show(" REPROBADO ");
+        }
+    }
+}
